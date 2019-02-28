448185c9568087af9ce8a6641e40a144b2e84267
colinbernard <colinjbernard@hotmail.com>
2019-02-28 10:12:54 -0800

    Created version and lang files

---
+++ lang/en/local_image_organizer.php
@@ -0,0 +1,3 @@
+<?php
+
+$string['pluginname'] = 'WCLN Image Organizer';
---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$plugin->version   = 2019022800; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2016112900; // Requires this Moodle version.
+$plugin->component = 'local_image_organizer'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v0.1';
